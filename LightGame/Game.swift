5725f6e79bc24bacd216e45fb460b2bae0ebd034
SergioStasevich <kangurek199@gmail.com>
2022-10-19 16:42:26 +0200

    workTime

--- LightGame/Game.swift
+++ LightGame/Game.swift
@@ -17,6 +17,7 @@ class Game {
     
     init (countItem:Int) {
         self.countItem = countItem
+        setupGame()
     }
     
     func setupGame() {
--- LightGame/GameViewController.swift
+++ LightGame/GameViewController.swift
@@ -18,7 +18,8 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupScreen ()
        
     }
     
