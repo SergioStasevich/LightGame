08f1716259f9cf25ae7cf8e347f1bbd4a5888258
SergioStasevich <kangurek1991@gmail.com>
2022-10-19 06:24:36 +0200

    Initial Commit

---
+++ LightGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LightGame
+//
+//  Created by Siarhei Stasevich on 19/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
